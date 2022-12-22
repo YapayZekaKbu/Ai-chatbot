ba6b2c2227b9286db83dd6f5f4eb36a181fffcb7
olaDEN <97802281+olaDEN@users.noreply.github.com>
2022-12-22 20:58:21 +0300

    Update Ai-Chatbot.py
    I modified the code, we need to specify time for listening so the microphone won't take long time while listening, short sentences work better with this model.

--- yapayZekaKbu/Ai-Chatbot.py
+++ yapayZekaKbu/Ai-Chatbot.py
@@ -1,5 +1,9 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import os
 import speech_recognition as sr
-import pyttsx3
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 r = sr.Recognizer()
 
@@ -8,8 +12,9 @@ def speechToText():
     try:
         with sr.Microphone() as source2:
             r.adjust_for_ambient_noise(source2, duration=0.2)
-            print("start speaking\n Listening... ")
-            audio2 = r.listen(source2)
+            print("Please start speaking, I am Listening...\n")
+            r.pause_threshold = 1
+            audio2 = r.listen(source2,timeout=3, phrase_time_limit=11)
             MyText = r.recognize_google(audio2)
             MyText = MyText.lower()
             return MyText
@@ -19,12 +24,7 @@ def speechToText():
     except sr.UnknownValueError:
         return "unknown error occurred"
     
-    
-    
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 
 ######################### DO NOT CHANGE#########################
 model_name = "microsoft/DialoGPT-medium"
@@ -36,10 +36,9 @@ model = AutoModelForCausalLM.from_pretrained(model_name)
 ## Do not remove the for loop ##
 for step in range(5):
 
-
     ### Burada, cümleyi kullanıcıdan yazarak aliyoruz ama biz mikrofondan alacağız sonra tekrar metne donusturecegiz
     ### Here,sentence is taken from user by typing, but we will take it from microphone and then convert it to text
-    print("1. text input\n 2. voice input")
+    print("1. text input\n2. voice input")
     n = int(input())
     if(n==1):
         text = input(">> You:")
@@ -74,4 +73,4 @@ for step in range(5):
 
     #### Output'u metin olarak değil, ses olarak cikartacagiz
     #### We will display the output as a voice not as a text 
-    print(f"DialoGPT: {output}")
+    print(f"DialoGPT: {output}\n")
